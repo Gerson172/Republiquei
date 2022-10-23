b9bd6105aaf549fef3e3da762b8d002ea49d9a4d
huser0 <hugo10sergio@gmail.com>
2022-10-23 12:45:48 -0300

    correção de commit

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,17 +1,10 @@
 import NavBar from "../../components/NavBar";
-import Skeleton from "../../components/Skeleton";
 
 const About = () => {
     return (
         <>
         <NavBar/>
         <div className="flex flex-col">
-        <Skeleton />
-        <Skeleton />
-        <Skeleton />
-        <Skeleton />
-        <Skeleton />
-        <Skeleton />
         </div>
         </>
     )
