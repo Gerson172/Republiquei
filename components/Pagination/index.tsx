613bf085b53db3b9d644d99954764d67096e78e7
huser0 <hugo10sergio@gmail.com>
2022-10-23 21:58:16 -0300

    correcao de build

--- components/Pagination/index.tsx
+++ components/Pagination/index.tsx
@@ -1,10 +1,11 @@
 function Pagination(){
+
     return (
 <nav aria-label="Page navigation example">
   <ul className="inline-flex items-center -space-x-px">
     <li>
       <a href="#" className="block py-2 px-3 ml-0 leading-tight text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-        <span className="sr-only">Previous</span>
+        <span  className="sr-only">Previous</span>
         <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
       </a>
     </li>
