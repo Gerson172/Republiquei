8a020f01691e13f282c6edd9cc60c010c03f4f1d
Claudinei Gomes <60714768+ClaudHelloworld@users.noreply.github.com>
2022-11-03 09:35:21 -0300

    add crud usuario

--- pages/register/index.tsx
+++ pages/register/index.tsx
@@ -1,13 +1,19 @@
+import CrudUsuario from "../../components/CrudUsuario";
+import NavBar from "../../components/NavBar";
 function Register(){
     return (
-        <div className="w-100 h-100 flex justify-center">
+        <>
+         <NavBar/>
+        {/* <div className="w-100 h-100 flex justify-center">
         <ul className="steps">
         <li className="step step-primary">Register</li>
         <li className="step step-primary">Choose plan</li>
         <li className="step">Purchase</li>
         <li className="step">Receive Product</li>
         </ul>
-        </div>
+        </div> */}
+        <CrudUsuario/>
+        </> 
     )
 
 }
