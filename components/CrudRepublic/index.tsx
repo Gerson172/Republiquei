c050c149b64edff9161732677ca7c2b4c35ec638
Claudinei Gomes <60714768+ClaudHelloworld@users.noreply.github.com>
2022-11-03 09:36:23 -0300

    Update index.tsx

--- components/CrudRepublic/index.tsx
+++ components/CrudRepublic/index.tsx
@@ -8,40 +8,34 @@ flex items-center justify-center">
           <form>
             <div className="grid md:grid-cols-2 md:gap-6">
               <div className="relative z-0 mb-6 w-full group">
-                <label htmlFor="small-input" className="block mb-2 text-sm font-medium text-gray-900">Título do anúncio do seu imóvel</label>
-                <input placeholder="EX: Apartamento, 4 quartos..." type="text" id="small-input" className="block p-2 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-xs focus:ring-blue-500 focus:border-blue-500" />
+                <label htmlFor="default-input" className="block mb-2 text-sm font-medium text-gray-900">Título do anúncio do seu imóvel</label>
+                <input placeholder="EX: Apartamento, 4 quartos..." type="text" id="small-input titulo-imovel" className="block p-2 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-xs focus:ring-blue-500 focus:border-blue-500" />
               </div>
+
+
               <div className="relative z-0 mb-6 w-full group">
-                <div>
-                  <label htmlFor="price" className="block text-sm font-medium text-gray-700">Valor</label>
-                  <div className="relative mt-1 rounded-md shadow-sm">
-                    <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                      <span className="text-gray-500 sm:text-sm">R$</span>
-                    </div>
-                    <input type="text" name="price" id="price" className="block w-full rounded-md border-gray-300 pl-7 pr-12 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="0.00 ( Valor separdado por ponto ou vírgulas )" />
-                    <div className="absolute inset-y-0 right-0 flex items-center">
-                      <label htmlFor="currency" className="sr-only">Currency</label>
-                    </div>
-                  </div>
-                </div>
+                <label htmlFor="small-input" className="block mb-2 text-sm font-medium text-gray-900">Valor</label>
+                <input placeholder="R$ 000,00 ou 0,000,00" type="number" id="small-input valor-imovel" className="block p-2 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-xs focus:ring-blue-500 focus:border-blue-500" />
               </div>
+
+
             </div>
             <div className="relative z-0 mb-6 w-full group">
               <label htmlFor="small-input" className="block mb-2 text-sm font-medium text-gray-900">Endereço completo</label>
-              <input placeholder="EX: Av. Rua São João N°33 Cond..." type="text" id="small-input" className="block p-2 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-xs focus:ring-blue-500 focus:border-blue-500" />
+              <input placeholder="EX: Av. Rua São João N°33 Cond..." type="text" id="small-input endereco-imovel" className="block p-2 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-xs focus:ring-blue-500 focus:border-blue-500" />
             </div>
             <div className="relative z-0 mb-6 w-full group">
               <label htmlFor="small-input" className="block mb-2 text-sm font-medium text-gray-900">O seu endereço possui alguma referência?</label>
-              <input placeholder="EX: Próximo a universidade de São Paulo..." type="text" id="small-input" className="block p-2 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-xs focus:ring-blue-500 focus:border-blue-500" />
+              <input placeholder="EX: Próximo a universidade de São Paulo..." type="text" id="small-input referencia-imovel" className="block p-2 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-xs focus:ring-blue-500 focus:border-blue-500" />
             </div>
             <div className="grid md:grid-cols-3 md:gap-6">
               <div className="relative z-0 mb-6 w-full group">
                 <label htmlFor="default-input" className="block mb-2 text-sm font-medium text-gray-900">Cidade</label>
-                <input placeholder="EX: São Paulo..." type="text" id="default-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
+                <input placeholder="EX: São Paulo..." type="text" id="default-input cidade-imovel" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
               </div>
               <div className="relative z-0 mb-6 w-full group">
                 <label htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900">Selecione o seu estado</label>
-                <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                <select id="countries estado-imovel" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
                   <option>SP</option>
                   <option>RJ</option>
                   <option>SC</option>
@@ -50,21 +44,21 @@ flex items-center justify-center">
               </div>
               <div className="relative z-0 mb-6 w-full group">
                 <label htmlFor="default-input" className="block mb-2 text-sm font-medium text-gray-900">CEP</label>
-                <input placeholder="EX: (11111-000) Com pontos e traçõs...." type="number" id="default-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
+                <input placeholder="EX: (11111-000) Com pontos e traçõs...." type="number" id="default-input cep-imovel" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
               </div>
             </div>
             <div className="grid md:grid-cols-3 md:gap-6">
               <div className="relative z-0 mb-6 w-full group">
                 <label className="block mb-2 text-sm font-medium text-gray-900" htmlFor="file_input">Subir imagens do imóvel</label>
-                <input className="block w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer focus:outline-none" id="file_input" type="file" />
+                <input className="block w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer focus:outline-none" id="file_input imagens-imovel" type="file" />
               </div>
               <div className="relative z-0 mb-6 w-full group">
                 <label htmlFor="default-input" className="block mb-2 text-sm font-medium text-gray-900">Telefone (Whatsapp)</label>
-                <input placeholder="EX: (99) 9 9999-999" type="tel" id="default-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
+                <input placeholder="EX: (99) 9 9999-999" type="tel" id="default-input telefone-imovel" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
               </div>
               <div className="relative z-0 mb-6 w-full group">
                 <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900">Descrição do Anúncio</label>
-                <textarea placeholder="Fale um pouco mais sobre o seu imóvel ;)" id="message" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"></textarea>
+                <textarea placeholder="Fale um pouco mais sobre o seu imóvel ;)" id="message desc-imovel" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"></textarea>
               </div>
             </div>
             <button type="submit" className="w-30 block bg-indigo-500 hover:bg-indigo-400 focus:bg-indigo-400 text-white font-semibold rounded-lg
