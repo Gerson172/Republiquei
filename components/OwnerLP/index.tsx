fe8491d80ef9a75a6ca7e5531658fb0fb34b0b10
Edhoni Bergue <89642122+edhonibergue@users.noreply.github.com>
2022-11-03 21:10:23 -0300

    Update index.tsx
    Alteração textual.

--- components/OwnerLP/index.tsx
+++ components/OwnerLP/index.tsx
@@ -15,12 +15,12 @@ const OwnerLP = () => {
                             <div className="flex flex-wrap flex-row-reverse items-center">
                                 <div className="md:w-1/2 lg:w-2/3 w-full lg:pl-20 md:pl-10 sm:pl-0 pl-0">
                                     <div className="py-2 text-color">
-                                        <h1 className="text-2xl lg:text-6xl tracking-tighter md:leading-snug f-f-l font-black">VOCÊ QUE TEM UM IMÓVEL, FAÇA PARTE DE ALGO MAIOR!</h1>
-                                        <h2 className="text-lg lg:text-3xl leading-7 md:leading-10 f-f-r py-8">Ganhe uma renda extra, incentivando o estudo de milhares de jovens por todo o Brasil!</h2>                               
+                                        <h1 className="text-2xl lg:text-6xl md:leading-snug tracking-tighter f-f-l font-black">Anuncie grátis para mais de 8 mil usuários</h1>
+                                        <h2 className="text-lg lg:text-3xl leading-7 md:leading-10 f-f-r py-8">Existem milhares de potenciais inquilinos na sua área à procura de um imóvel para chamar de lar. Com a Republiquei você pode atingir o universitário que mais combina com o seu imóvel.</h2>                               
                                     </div>
                                 </div>
                                 <div className="lg:w-1/3 md:w-1/2 w-full relative h-96 flex items-end justify-center">
-                                    <img alt="" className="absolute w-full h-full inset-0 object-cover object-center rounded-md" src="https://images.unsplash.com/photo-1507209575474-fa61e9d3086b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80" />
+                                    <img alt="" className="absolute w-full h-full inset-0 object-cover object-center rounded-md" src="https://img.freepik.com/fotos-gratis/composicao-de-negocios-com-estilo-moderno_23-2147900183.jpg?w=1480&t=st=1667520153~exp=1667520753~hmac=a0598574b61ae73528417fd61d55a4f68ec0569b47a3618b88bf80c519b11204" />
                                 </div>
                             </div>
                         </div>
@@ -31,7 +31,7 @@ const OwnerLP = () => {
                             <div className="flex flex-wrap items-center pb-12">
                                 <div className="md:w-1/2 lg:w-2/3 w-full xl:pr-20 md:pr-6">
                                     <div className="py-2 text-color">
-                                        <h1 className="text-2xl lg:text-6xl md:leading-snug tracking-tighter f-f-l font-black"> FAÇA A DIFERENÇA NA VIDA DE OUTRAS PESSOAS!</h1>
+                                        <h1 className="text-2xl lg:text-6xl md:leading-snug tracking-tighter f-f-l font-black">Alugue seu imóvel sem burocracias</h1>
                                         <div className="flex items-center cursor-pointer pb-4 md:pb-0 mt-8">
                                             <button type="button" className="text-white bg-rose-500 hover:bg-rose-600 focus:ring-4 focus:outline-none focus:ring-rose-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2">
                                                 <span>Clique e saiba mais</span>
@@ -42,7 +42,7 @@ const OwnerLP = () => {
                                     </div>
                                 </div>
                                 <div className="lg:w-1/3 md:w-1/2 w-full relative h-96 flex items-end justify-center">
-                                    <img alt="" className="absolute w-full h-full inset-0 object-cover object-center rounded-md" src="https://images.unsplash.com/photo-1544027993-37dbfe43562a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" />                          
+                                    <img alt="" className="absolute w-full h-full inset-0 object-cover object-center rounded-md" src="https://img.freepik.com/fotos-gratis/foto-aproximada-de-uma-pessoa-pensando-em-comprar-ou-vender-uma-casa_181624-24928.jpg?w=1380&t=st=1667520339~exp=1667520939~hmac=262113300eab6a842f0e4ae6eb209493b1c77635500b2cf31d78807025a62f2e" />                          
                                 </div>
                             </div>
                         </div>
